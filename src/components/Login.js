94f6e25fe54c1e5c86a1468c5c3c2792e28920a6
tntn245 <tranngoctonhu2405@gmail.com>
2024-06-27 19:11:11 +0700

    fetch api for login, register

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,38 @@ const clientID =
 const clientSecret = "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";
 
 function Login({ onLogin }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
   const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://127.0.0.1:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.status === 200) {
+        setMessage(data.message); // Thông báo đăng nhập thành công
+        navigate('/dashboard')
+        // Thực hiện hành động sau khi đăng nhập thành công (ví dụ: chuyển hướng trang)
+      } else {
+        setMessage(data.message); // Thông báo lỗi đăng nhập
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setMessage('An error occurred. Please try again.');
+    }
+  };
 
   const handleGoogleLoginSuccess = (credentialResponse) => {
     const token = credentialResponse?.credential;
@@ -27,6 +58,25 @@ function Login({ onLogin }) {
 
   return (
     <div>
+    <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      {message && <p>{message}</p>}
+
       <GoogleOAuthProvider clientId="826103606588-eqe5jffn43d1ge68f63bcnr1dld44lun.apps.googleusercontent.com">
         <GoogleLogin
           onSuccess={handleGoogleLoginSuccess}
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,14 +1,37 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordMatch, setPasswordMatch] = useState(true); // State để kiểm tra mật khẩu khớp nhau
+
   const [message, setMessage] = useState('');
+  const [showOTPInput, setShowOTPInput] = useState(false);
+  const [OTPSent, setOTPSent] = useState('');
+  const [OTPInput, setOTPInput] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleConfirmPasswordChange = (e) => {
+    const confirmPwd = e.target.value;
+    setConfirmPassword(confirmPwd);
+    setPasswordMatch(password === confirmPwd);
+  };
 
   const handleRegister = async (e) => {
     e.preventDefault();
+    setOTPInput('');
+    setMessage('');
+    setShowOTPInput(false); 
 
+    if (!passwordMatch) {
+        return;
+      }
+  
     try {
-      const response = await fetch('http://127.0.0.1:5000/register', {
+      const response = await fetch('http://127.0.0.1:5000/checkEmail', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -18,6 +41,29 @@ function Register() {
 
       const data = await response.json();
       setMessage(data.message);
+      setOTPSent(data.otp);
+      if (response.status === 200) {
+        setShowOTPInput(true); 
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setMessage('An error occurred. Please try again.');
+    }
+  };
+
+  const handleVerifyOTP = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/verifyOTP', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password, OTPSent, OTPInput }),
+      });
+
+      const data = await response.json();
+      setMessage(data.message);
+      navigate('/')
     } catch (error) {
       console.error('Error:', error);
       setMessage('An error occurred. Please try again.');
@@ -34,9 +80,39 @@ function Register() {
           onChange={(e) => setEmail(e.target.value)}
           required
         />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Confirm your password"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+          required
+          style={{ borderColor: passwordMatch ? 'black' : 'red' }} 
+        />
         <button type="submit">Register</button>
       </form>
+      {!passwordMatch && <p style={{ color: 'red' }}>Passwords do not match.</p>}
       {message && <p>{message}</p>}
+      
+      {/* Hiển thị OTP input nếu gửi OTP thành công */}
+      {showOTPInput && (
+        <div>
+          <input
+            type="text"
+            placeholder="Enter OTP"
+            value={OTPInput}
+            onChange={(e) => setOTPInput(e.target.value)}
+            required
+          />
+          <button onClick={handleVerifyOTP}>Verify OTP</button>
+        </div>
+      )}
     </div>
   );
 }
