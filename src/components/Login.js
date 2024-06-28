28bb9c6dd518bb95f59f8855507a2eb5cf454c4a
tntn245 <tranngoctonhu2405@gmail.com>
2024-06-28 16:20:04 +0700

    css login, register

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,9 +5,9 @@ import axios from "../api/axios.js";
 import "../style/Login.css";
 
 function Login({ onLogin }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   const navigate = useNavigate();
 
@@ -15,29 +15,35 @@ function Login({ onLogin }) {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://weatherweb-1s99.onrender.com/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
+      const response = await fetch(
+        "https://weatherweb-1s99.onrender.com/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
 
       const data = await response.json();
 
       if (response.status === 200) {
         onLogin(data.access_token);
-        navigate('/dashboard')
+        navigate("/dashboard");
       } else {
         setMessage(data.message);
       }
-
     } catch (error) {
-      console.error('Error:', error);
-      setMessage('An error occurred. Please try again.');
+      console.error("Error:", error);
+      setMessage("An error occurred. Please try again.");
     }
   };
-  
+
+  const handleNavRegister = () => {
+    navigate("/register");
+  };
+
   const handleGoogleLoginSuccess = (credentialResponse) => {
     const token = credentialResponse?.credential;
     if (token) {
@@ -45,8 +51,8 @@ function Login({ onLogin }) {
         .post("https://weatherweb-1s99.onrender.com/google-login", { token })
         .then((response) => {
           const jwtToken = response.data.access_token;
-          onLogin(jwtToken);  // Lưu JWT vào localStorage
-          navigate('/dashboard');
+          onLogin(jwtToken); // Lưu JWT vào localStorage
+          navigate("/dashboard");
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
@@ -56,30 +62,35 @@ function Login({ onLogin }) {
 
   return (
     <div className="login-container">
-    <form onSubmit={handleLogin} className="login-form">
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-      <Link to="/register">Register</Link>
+      <div className="card">
+        <form onSubmit={handleLogin} className="login-form">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
 
-      <div>
-       <p>{message}</p>
-       </div>
+        <p>{message}</p>
 
-      {/* <GoogleOAuthProvider clientId="826103606588-eqe5jffn43d1ge68f63bcnr1dld44lun.apps.googleusercontent.com">
+        <div className="divider">
+          <span className="divider-text">No account?</span>
+        </div>
+        <button className="another-button" onClick={handleNavRegister}>
+          Register
+        </button>
+
+        {/* <GoogleOAuthProvider clientId="826103606588-eqe5jffn43d1ge68f63bcnr1dld44lun.apps.googleusercontent.com">
         <GoogleLogin
           onSuccess={handleGoogleLoginSuccess}
           onError={() => {
@@ -87,6 +98,7 @@ function Login({ onLogin }) {
           }}
         />
       </GoogleOAuthProvider> */}
+      </div>
     </div>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../style/Register.css";
 
 function Register() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordMatch, setPasswordMatch] = useState(true); // State để kiểm tra mật khẩu khớp nhau
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [showOTPInput, setShowOTPInput] = useState(false);
-  const [OTPSent, setOTPSent] = useState('');
-  const [OTPInput, setOTPInput] = useState('');
+  const [OTPSent, setOTPSent] = useState("");
+  const [OTPInput, setOTPInput] = useState("");
 
   const navigate = useNavigate();
 
@@ -23,102 +23,124 @@ function Register() {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    setOTPInput('');
-    setMessage('');
-    setShowOTPInput(false); 
+    setOTPInput("");
+    setMessage("");
+    setShowOTPInput(false);
 
     if (!passwordMatch) {
-        return;
-      }
-  
+      return;
+    }
+
     try {
-      const response = await fetch('https://weatherweb-1s99.onrender.com/checkEmail', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-      });
+      const response = await fetch(
+        "https://weatherweb-1s99.onrender.com/checkEmail",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email }),
+        }
+      );
 
       const data = await response.json();
-      setMessage(data.message);
+      // setMessage(data.message);
       setOTPSent(data.otp);
       if (response.status === 200) {
-        setShowOTPInput(true); 
+        setShowOTPInput(true);
       }
     } catch (error) {
-      console.error('Error:', error);
-      setMessage('An error occurred. Please try again.');
+      console.error("Error:", error);
+      setMessage("An error occurred. Please try again.");
     }
   };
 
   const handleVerifyOTP = async () => {
     try {
-      const response = await fetch('https://weatherweb-1s99.onrender.com/verifyOTP', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password, OTPSent, OTPInput }),
-      });
+      const response = await fetch(
+        "https://weatherweb-1s99.onrender.com/verifyOTP",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password, OTPSent, OTPInput }),
+        }
+      );
 
       const data = await response.json();
       setMessage(data.message);
-      navigate('/')
+      navigate("/");
     } catch (error) {
-      console.error('Error:', error);
-      setMessage('An error occurred. Please try again.');
+      console.error("Error:", error);
+      setMessage("An error occurred. Please try again.");
     }
+  };
+
+  const handleNavLogin = () => {
+    navigate("/");
   };
 
   return (
     <div className="register-container">
-      <form onSubmit={handleRegister} className="register-form">
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Confirm your password"
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-          required
-          style={{ borderColor: passwordMatch ? '' : 'red' }} 
-        />
-        <button type="submit">Register</button>
-      </form>
-      {!passwordMatch && <p style={{ color: 'red' }}>Passwords do not match.</p>}
-      {message && <p>{message}</p>}
-      
-      {/* Hiển thị OTP input nếu gửi OTP thành công */}
-      {showOTPInput && (
-        <div className="otp-container">
-          <input className='otp-input'
-            type="text"
-            placeholder="Enter OTP"
-            value={OTPInput}
-            onChange={(e) => setOTPInput(e.target.value)}
+      <div className="card">
+        <form onSubmit={handleRegister} className="register-form">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
-          <button onClick={handleVerifyOTP} className="otp-button">Verify OTP</button>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Confirm your password"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            required
+            style={{ borderColor: passwordMatch ? "" : "red" }}
+          />
+          <button type="submit">Register</button>
+
+          {!passwordMatch && (
+            <p style={{ color: "red" }}>Passwords do not match.</p>
+          )}
+
+          {message && <p>{message}</p>}
+
+          {showOTPInput && (
+            <div className="otp-container">
+              <p>OTP sent to your email!</p>
+              <input
+                className="otp-input"
+                type="text"
+                placeholder="Enter OTP"
+                value={OTPInput}
+                onChange={(e) => setOTPInput(e.target.value)}
+                required
+              />
+              <button onClick={handleVerifyOTP} className="otp-button">
+                Verify OTP
+              </button>
+            </div>
+          )}
+        </form>
+
+        <div className="divider">
+          <span className="divider-text">No account?</span>
         </div>
-      )}
-
-      <Link to="/">Back to Login</Link>
-
+        <button className="another-button" onClick={handleNavLogin}>
+          Back to Login
+        </button>
       </div>
-
+    </div>
   );
 }
 
